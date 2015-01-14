580dc1ce74ea9c514e6679ad449077bcf329318b
Georg Sieber <sieber@cern.ch>
2015-01-14 14:02:32 +0100

    modify to path

--- skim_pfjets.py
+++ skim_pfjets.py
@@ -24,7 +24,7 @@ process = cms.Process("SKIM")
 process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring(filename)
 )
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(100) )
 print "GT: {0}".format(globaltag)
 print "TYPE: {0}".format(datatype)
 
@@ -47,7 +47,6 @@ if is_data:
    process.source.lumisToProcess = LumiList.LumiList(filename = 'Cert_190456-208686_8TeV_22Jan2013ReReco_Collisions12_JSON.txt').getVLuminosityBlockRange()
 
 import HLTrigger.HLTfilters.hltHighLevel_cfi as hlt
-# accept if 'path_1' succeeds
 process.hlt_pfjets = hlt.hltHighLevel.clone(
     HLTPaths = ["HLT_PFJet*_v*"],
     throw = False
@@ -55,8 +54,60 @@ process.hlt_pfjets = hlt.hltHighLevel.clone(
 
 process.hlt_pfjetspath = cms.Path(process.hlt_pfjets)
 
-process.load("RecoMET.METFilters.metFilters_cff")
-process.metFilterPath = cms.Path(process.metFilters)
+
+## The iso-based HBHE noise filter ___________________________________________||
+process.load('CommonTools.RecoAlgos.HBHENoiseFilter_cfi')
+process.load('CommonTools.RecoAlgos.HBHENoiseFilterResultProducer_cfi')
+## The CSC beam halo tight filter ____________________________________________||
+process.load('RecoMET.METFilters.CSCTightHaloFilter_cfi')
+## The HCAL laser filter _____________________________________________________||
+process.load('RecoMET.METFilters.hcalLaserEventFilter_cfi')
+## The ECAL dead cell trigger primitive filter _______________________________||
+process.load('RecoMET.METFilters.EcalDeadCellTriggerPrimitiveFilter_cfi')
+## The EE bad SuperCrystal filter ____________________________________________||
+process.load('RecoMET.METFilters.eeBadScFilter_cfi')
+## The ECAL laser correction filter
+process.load('RecoMET.METFilters.ecalLaserCorrFilter_cfi')
+## The Good vertices collection needed by the tracking failure filter ________||
+process.goodVertices = cms.EDFilter(
+  "VertexSelector",
+  filter = cms.bool(False),
+  src = cms.InputTag("offlinePrimaryVertices"),
+  cut = cms.string("!isFake && ndof > 4 && abs(z) <= 24 && position.rho < 2")
+)
+## The tracking failure filter _______________________________________________||
+process.load('RecoMET.METFilters.trackingFailureFilter_cfi')
+## The tracking POG filters __________________________________________________||
+process.load('RecoMET.METFilters.trackingPOGFilters_cff')
+## NOTE: to make tagging mode of the tracking POG filters (three of them), please do:
+##    manystripclus53X.taggedMode = cms.untracked.bool(True)
+##    manystripclus53X.forcedValue = cms.untracked.bool(False)
+##    toomanystripclus53X.taggedMode = cms.untracked.bool(True)
+##    toomanystripclus53X.forcedValue = cms.untracked.bool(False)
+##    logErrorTooManyClusters.taggedMode = cms.untracked.bool(True)
+##    logErrorTooManyClusters.forcedValue = cms.untracked.bool(False)
+## Also the stored boolean for the three filters is opposite to what we usually
+## have for other filters, i.e., true means rejected bad events while false means 
+## good events.
+process.HBHENoiseFilter.taggingMode = cms.bool(True)
+process.CSCTightHaloFilter.taggingMode = cms.bool(True)
+process.hcalLaserEventFilter.taggingMode = cms.bool(True)
+process.EcalDeadCellTriggerPrimitiveFilter.taggingMode = cms.bool(True)
+process.trackingFailureFilter.taggingMode = cms.bool(True)
+process.eeBadScFilter.taggingMode = cms.bool(True)
+process.ecalLaserCorrFilter.taggingMode = cms.bool(True)
+# process.trkPOGFilters.taggingMode = cms.bool(True)
+# TODO: trkPOGFilters are not putting a product in the event, so no passing to FilterVertexSummary possible
+process.metFilters = cms.Sequence(
+   process.HBHENoiseFilterResultProducer + process.HBHENoiseFilter +
+   process.CSCTightHaloFilter +
+   process.hcalLaserEventFilter +
+   process.EcalDeadCellTriggerPrimitiveFilter +
+   process.goodVertices * process.trackingFailureFilter +
+   process.eeBadScFilter +
+   process.ecalLaserCorrFilter +
+   process.trkPOGFilters
+)
 #-------------------------------------------------------------------------------
 # Filter Results
 #-------------------------------------------------------------------------------
@@ -87,7 +138,6 @@ process.kappatuple.Metadata.hltWhitelist = cms.vstring(
     "^HLT_PFJet[0-9]+(U)?(_NoJetID)?(_v[[:digit:]]+)?$",
 )
 process.kappatuple.Metadata.hltBlacklist = cms.vstring()
-process.pathSkim = cms.Path(process.kappatuple)
 
 #-------------------------------------------------------------------------------
 # Output
@@ -105,7 +155,6 @@ process.pathSkim = cms.Path(process.kappatuple)
 #-------------------------------------------------------------------------------
 # JEC
 #-------------------------------------------------------------------------------
-# TODO: USE JEC from Winter14 only on data
 process.load("CondCore.DBCommon.CondDBCommon_cfi")
 process.load('JetMETCorrections.Configuration.DefaultJEC_cff')
 process.load('JetMETCorrections.Configuration.JetCorrectionProducers_cff')
@@ -143,15 +192,14 @@ else:
     # In this case the JEC from the GT (START_XYZ) should be used
     pass
 
-process.jec = cms.Path(process.ak5PFJetsL1FastL2L3Residual + process.ak7PFJetsL1FastL2L3Residual)
+process.jec = cms.Sequence(process.ak5PFJetsL1FastL2L3Residual + process.ak7PFJetsL1FastL2L3Residual)
 
 #-------------------------------------------------------------------------------
 # Process schedule
 #-------------------------------------------------------------------------------
 
-process.schedule = cms.Schedule(
-        process.metFilterPath,
-        process.hlt_pfjetspath,
-        process.jec,
-        process.pathSkim,
-        )
+process.skim_path = cms.Path(process.metFilters *
+                             process.hlt_pfjets *
+                             process.jec *
+                             process.kappatuple
+                             )
