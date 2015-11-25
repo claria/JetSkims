ff1039273443bbe0e0f185752c08ff533db27fb9
Georg Sieber <g.sieber@gmail.com>
2015-11-25 16:39:19 +0100

    first config try for 13 TeV skim

--- 13TeV/skim_pfjets.py
+++ 13TeV/skim_pfjets.py
@@ -35,12 +35,9 @@ print "Datatype: {0}".format(datatype)
 # Global Tag
 #-------------------------------------------------------------------------------
 process.load("FWCore.MessageService.MessageLogger_cfi")
-process.load('Configuration.StandardSequences.Services_cff')
 process.load('Configuration.StandardSequences.MagneticField_38T_cff')
 process.load('Configuration.Geometry.GeometryRecoDB_cff')
-process.load('Configuration.Geometry.GeometryPilot2_cff')
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff')
-process.load("Configuration.StandardSequences.Reconstruction_cff")
 process.GlobalTag.globaltag = globaltag
 
 
@@ -57,7 +54,7 @@ process.kappatuple = cms.EDAnalyzer('KTuple',
 )
 process.kappatuple.verbose = cms.int32(0)
 process.kappatuple.active = cms.vstring(
-    'TrackSummary', 'VertexSummary', 'FilterSummary', 'MET', 'BasicJets', 'PileupDensity'
+    'TrackSummary', 'VertexSummary', 'FilterSummary', 'MET', 'BasicJets'
 )
 if is_data:
     process.kappatuple.active.append('DataInfo')
@@ -77,6 +74,15 @@ process.kappatuple.Info.hltBlacklist = cms.vstring()
 #--------------------------------------------------------------
 # PFCandidates
 #--------------------------------------------------------------
+# Primary Input Collections ###################################################
+## miniAOD has NOT been tested, I'm just guessing names - MF@20150907
+input_PFCandidates = 'particleFlow'
+input_PFCandidatePtrs = 'particleFlowPtrs'
+input_PrimaryVertices = 'goodOfflinePrimaryVertices'
+
+
+#  PFCandidates  ###################################################
+## Good offline PV selection: 
 from PhysicsTools.SelectorUtils.pvSelector_cfi import pvSelector
 process.goodOfflinePrimaryVertices = cms.EDFilter('PrimaryVertexObjectFilter',
     filterParams = pvSelector.clone(maxZ = 24.0),  # ndof >= 4, rho <= 2
@@ -90,94 +96,89 @@ process.load("CommonTools.ParticleFlow.pfParticleSelection_cff")
 
 
 ## pf candidate configuration for everything but CHS jets
-process.pfPileUpIso.PFCandidates = 'particleFlow'
-process.pfPileUpIso.Vertices = 'goodOfflinePrimaryVertices'
-process.pfPileUpIso.checkClosestZVertex = True
-process.pfNoPileUpIso.bottomCollection = 'particleFlow'
+process.pfPileUpIso.PFCandidates        = cms.InputTag(input_PFCandidates)
+process.pfPileUpIso.Vertices            = cms.InputTag(input_PrimaryVertices)
+process.pfPileUpIso.checkClosestZVertex    = True
+process.pfNoPileUpIso.bottomCollection    = cms.InputTag(input_PFCandidates)
 
 
 ## pf candidate configuration for deltaBeta corrections for muons and electrons 
-process.pfNoPileUpChargedHadrons  = process.pfAllChargedHadrons.clone()
-process.pfNoPileUpNeutralHadrons = process.pfAllNeutralHadrons.clone()
-process.pfNoPileUpPhotons = process.pfAllPhotons.clone()
-process.pfPileUpChargedHadrons = process.pfAllChargedHadrons.clone(src = 'pfPileUpIso')
+process.pfNoPileUpChargedHadrons    = process.pfAllChargedHadrons.clone()
+process.pfNoPileUpNeutralHadrons    = process.pfAllNeutralHadrons.clone()
+process.pfNoPileUpPhotons            = process.pfAllPhotons.clone()
+process.pfPileUpChargedHadrons        = process.pfAllChargedHadrons.clone(src = 'pfPileUpIso')
 
 ## pf candidate configuration for CHS jets
-process.pfPileUp.Vertices				= 'goodOfflinePrimaryVertices'
-process.pfPileUp.checkClosestZVertex	= False
+process.pfPileUp.Vertices                = cms.InputTag(input_PrimaryVertices)
+process.pfPileUp.checkClosestZVertex    = False
 
 # Modifications for new particleFlow Pointers
-process.pfPileUp.PFCandidates = cms.InputTag("particleFlowPtrs")
-process.pfPileUpIso.PFCandidates = cms.InputTag("particleFlowPtrs")
-process.pfNoPileUp.bottomCollection = cms.InputTag("particleFlowPtrs")
-process.pfNoPileUpIso.bottomCollection = cms.InputTag("particleFlowPtrs")
-#process.pfJetTracksAssociatorAtVertex.jets= cms.InputTag("ak5PFJets")
-process.path *= (process.goodOfflinePrimaryVertices * process.pfParticleSelectionSequence
+process.pfPileUp.PFCandidates = cms.InputTag(input_PFCandidatePtrs)
+process.pfPileUpIso.PFCandidates = cms.InputTag(input_PFCandidatePtrs)
+process.pfNoPileUp.bottomCollection = cms.InputTag(input_PFCandidatePtrs)
+process.pfNoPileUpIso.bottomCollection = cms.InputTag(input_PFCandidatePtrs)
+process.path *= (
+    process.goodOfflinePrimaryVertices
+    * process.pfParticleSelectionSequence
 )
-
 #--------------------------------------------------------------
 # Jets
 #--------------------------------------------------------------
 
+#  Jets  ###########################################################
 # Kappa jet processing
-process.kappaTuple.Jets.minPt = 5.0
-process.kappaTuple.Jets.taggers = cms.vstring()
+process.kappatuple.BasicJets.minPt = 5.0
+process.kappatuple.BasicJets.taggers = cms.vstring()
 
-# containers of objects to process
+# containers for objects to process
 jet_resources = []
 cmssw_jets = {}  # algoname: cmssw module
 kappa_jets = {}  # algoname: kappa jet config
 
 # GenJets
-if not data:
-	process.load('RecoJets.Configuration.GenJetParticles_cff')
-	process.load('RecoJets.JetProducers.ak5GenJets_cfi')
-	jet_resources.append(process.genParticlesForJetsNoNu)
-	process.kappaTuple.active += cms.vstring('LV')
-	process.kappaTuple.LV.whitelist = cms.vstring('ak5GenJetsNoNu') #default?
-	genbase_jet = process.ak5GenJets.clone(src=cms.InputTag("genParticlesForJetsNoNu"), doAreaFastjet=True)
+if not is_data:
+    process.load('RecoJets.Configuration.GenJetParticles_cff')
+    process.load('RecoJets.JetProducers.ak5GenJets_cfi')
+    jet_resources.append(process.genParticlesForJetsNoNu)
+    process.kappatuple.active += cms.vstring('LV')
+    process.kappatuple.LV.whitelist = cms.vstring('ak5GenJetsNoNu') #default?
+    genbase_jet = process.ak5GenJets.clone(src=cms.InputTag("genParticlesForJetsNoNu"), doAreaFastjet=True)
 
 ## PFBRECO?
 process.load("RecoJets.JetProducers.ak5PFJets_cfi")
 pfbase_jet = process.ak5PFJets.clone(srcPVs = 'goodOfflinePrimaryVertices', doAreaFastjet=True)
 
-## PUPPI
-# creates reweighted PFCandidates collection 'puppi'
-process.load('CommonTools.PileupAlgos.Puppi_cff')
-jet_resources.append(process.puppi)
-if miniaod:
-	process.puppi.candName = cms.InputTag('packedPFCandidates')
-	process.puppi.vertexName = cms.InputTag('offlineSlimmedPrimaryVertices')
-
 # create Jet variants
-for param in (4, 5, 8):
-	# PFJets
-	for algo, input_tag in (("", 'particleFlow'), ("CHS", 'pfNoPileUp'), ("Puppi", 'puppi')):
-		variant_name = "ak%dPFJets%s" % (param, algo)
-		variant_mod = pfbase_jet.clone(src=cms.InputTag(input_tag), rParam=param/10.0)
-		cmssw_jets[variant_name] = variant_mod
-		# Full Kappa jet definition
-		kappa_jets["ak%dPFJets%s"%(param, algo)] = cms.PSet(
-			src = cms.InputTag(variant_name),
-			PUJetID = cms.InputTag("ak%dPF%sPuJetMva" % (param, algo)),
-			PUJetID_full = cms.InputTag("full"),
-			QGtagger = cms.InputTag("AK%dPFJets%sQGTagger" % (param, algo)),
-			Btagger = cms.InputTag("ak%dPF%s" % (param, algo)),
-		)
-	# GenJets
-	if not data:
-		for collection in ("NoNu",): # TODO: add "NoMuNoNu", "" ?
-			variant_name = "ak%sGenJets%s" % (param, collection)
-			variant_mod = genbase_jet.clone(rParam=param/10.0)
-			cmssw_jets[variant_name] = variant_mod
-			# GenJets are just KLVs
-			process.kappaTuple.LV.whitelist += cms.vstring(variant_name)
+for param in (4, 5, 7, 8):
+    # PFJets
+    algos_and_tags = [("", input_PFCandidates), ("CHS", 'pfNoPileUp')]
+    for algo, input_tag in algos_and_tags:
+        variant_name = "ak%dPFJets%s" % (param, algo)
+        variant_mod = pfbase_jet.clone(src=cms.InputTag(input_tag), rParam=param/10.0)
+        cmssw_jets[variant_name] = variant_mod
+        # Full Kappa jet definition
+        kappa_jets["ak%dPFJets%s"%(param, algo)] = cms.PSet(
+            src = cms.InputTag(variant_name),
+            PUJetID = cms.InputTag("ak%dPF%sPuJetMva" % (param, algo)),
+            PUJetID_full = cms.InputTag("full"),
+            QGtagger = cms.InputTag("AK%dPFJets%sQGTagger" % (param, algo)),
+            Btagger = cms.InputTag("ak%dPF%s" % (param, algo)),
+        )
+    # GenJets
+    if not is_data:
+        for collection in ("NoNu",): # TODO: add "NoMuNoNu", "" ?
+            variant_name = "ak%sGenJets%s" % (param, collection)
+            variant_mod = genbase_jet.clone(rParam=param/10.0)
+            cmssw_jets[variant_name] = variant_mod
+            # GenJets are just KLVs
+            process.kappatuple.LV.whitelist += cms.vstring(variant_name)
 # mount generated jets for processing
 for name, jet_module in cmssw_jets.iteritems():
-	setattr(process, name, jet_module)
+    setattr(process, name, jet_module)
 for name, pset in kappa_jets.iteritems():
-	setattr(process.kappaTuple.Jets, name, pset)
-process.path *= reduce(lambda a, b: a * b, jet_resources) * reduce(lambda a, b: a * b, sorted(cmssw_jets.values()))
+    setattr(process.kappatuple.BasicJets, name, pset)
+process.path *= reduce(lambda a, b: a * b, jet_resources + sorted(cmssw_jets.values()))
+
 
 #-------------------------------------------------------------------------------
 # Pileup Density
@@ -185,8 +186,8 @@ process.path *= reduce(lambda a, b: a * b, jet_resources) * reduce(lambda a, b: 
 from RecoJets.JetProducers.kt4PFJets_cfi import kt4PFJets
 process.pileupDensitykt6PFJets = kt4PFJets.clone(rParam=0.6, doRhoFastjet=True, Rho_EtaMax=2.5)
 
-process.kappaTuple.active += cms.vstring('Jets', 'PileupDensity')
-process.kappaTuple.PileupDensity.rename = cms.vstring("fixedGridRhoFastjetAll => pileupDensity")
+process.kappatuple.active += cms.vstring('Jets', 'PileupDensity')
+process.kappatuple.PileupDensity.rename = cms.vstring("fixedGridRhoFastjetAll => pileupDensity")
 
 process.path *= (
     process.pileupDensitykt6PFJets
@@ -218,7 +219,7 @@ print "CMSSW producers:"
 for p in str(process.path).split('+'):
     print "  %s" % p
 print "Kappa producers:"
-for p in sorted(process.kappaTuple.active):
+for p in sorted(process.kappatuple.active):
     print "  %s" % p
 print "---------------------------------"
 
